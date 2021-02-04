fefa9e8eb832d7db86bcecccfce3d18b8cd90528
Documenter.jl <documenter@juliadocs.github.io>
2021-02-04 04:28:38 +0000

    build based on 1b0fad3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PkgTest","category":"page"},{"location":"#PkgTest","page":"Home","title":"PkgTest","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PkgTest]","category":"page"}]
+}
